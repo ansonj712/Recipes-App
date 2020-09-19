be828888f9552387f92064504fa160dd14a6787a
Anson Jiang <anson.jiang712@gmail.com>
2020-09-18 21:37:46 -0400

    Move files

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use('/api/ingredients', ingredientsRouter);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
